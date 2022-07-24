13df0290e7149d164ab611147e19b9f6a80b1b26
Shynghys <shynghys@mail.ru>
2022-07-25 03:20:40 +0600

    slider flickity

--- src/pages/main/Index.vue
+++ src/pages/main/Index.vue
@@ -1,51 +1,7 @@
 <template>
 	<div class="mainpage">
 		<the-filter :menuStatus="menuStatus" :toggle="toggleMenuStatus" />
-		<div class="hero">
-			<div class="slider">
-				<p class="slider__text">
-					Открытие 40-го Mobil 1 Center в г. Алматы
-				</p>
-				<img
-					src="@/assets/img/pages/mainpage/Rectangle 28.png"
-					alt=""
-					class="slider__img"
-				/>
-			</div>
-
-			<div class="podbor">
-				<div class="podbor__card">
-					<div>
-						<div class="podbor__title">Подбор по модели</div>
-						<div class="podbor__desc">
-							Подбор запчастей по модели автомобиля
-						</div>
-						<button class="podbor__btn">Попробовать</button>
-					</div>
-					<div>
-						<img
-							src="@/assets/img/pages/mainpage/blue-car-sellpage-audi@2x 1.svg"
-							alt=""
-						/>
-					</div>
-				</div>
-				<div class="podbor__card">
-					<div>
-						<div class="podbor__title">Подбор масла</div>
-						<div class="podbor__desc">
-							Подбор масел по VIN-коду и модели автомобиля
-						</div>
-						<button class="podbor__btn">Попробовать</button>
-					</div>
-					<div>
-						<img
-							src="@/assets/img/pages/mainpage/maslo.svg"
-							alt=""
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
+		<the-hero />
 		<div class="brands">
 			<div class="brands__block">
 				<div class="brands__title">Бренды</div>
@@ -55,9 +11,16 @@
 						<img src="@/assets/img/icons/arrow.svg" alt="" /></div
 				></a>
 			</div>
-
-			<div class="brands__slider">
-				<div v-for="(item, index) in slider2" :key="index">
+			<flickity
+				class="flickity"
+				ref="flickity"
+				:options="flickityOptions"
+			>
+				<div
+					v-for="(item, index) in slider2"
+					:key="index"
+					class="carousel-cell"
+				>
 					<a href="" class="brands__item">
 						<img
 							class="brands__img"
@@ -69,26 +32,24 @@
 						<p class="brands__text">{{ item.text }}</p></a
 					>
 				</div>
-			</div>
+			</flickity>
 			<dropdown-modal v-if="menuStatus"></dropdown-modal>
 		</div>
 	</div>
 </template>
 <script>
 import TheFilter from "./components/TheFilter.vue";
+import TheHero from "./components/TheHero.vue";
 import DropdownModal from "./components/DropdownModal.vue";
+import Flickity from "vue-flickity";
+
 export default {
 	name: "MainIndex",
 
-	components: { TheFilter, DropdownModal },
+	components: { TheFilter, DropdownModal, Flickity, TheHero },
 	data() {
 		return {
 			menuStatus: false,
-			slider: [
-				"@/assets/img/pages/mainpage/Rectangle 28.png",
-				"@/assets/img/pages/mainpage/Rectangle 28.png",
-				"@/assets/img/pages/mainpage/Rectangle 28.png",
-			],
 			slider2: [
 				{ text: "AD BATTERY", link: "", img: "brand-logo.png" },
 				{ text: "MOBIL", link: "", img: "brand-logo1.png" },
@@ -96,13 +57,35 @@ export default {
 				{ text: "555", link: "", img: "brand-card3.png" },
 				{ text: "DENSO", link: "", img: "brand-card4.png" },
 				{ text: "FRAM", link: "", img: "brand-card5.png" },
+				{ text: "AD BATTERY", link: "", img: "brand-logo.png" },
+				{ text: "MOBIL", link: "", img: "brand-logo1.png" },
+				{ text: "MANN-FILTER", link: "", img: "brand-logo2.png" },
+				{ text: "555", link: "", img: "brand-card3.png" },
+				{ text: "DENSO", link: "", img: "brand-card4.png" },
+				{ text: "FRAM", link: "", img: "brand-card5.png" },
 			],
+			flickityOptions: {
+				prevNextButtons: true,
+				wrapAround: true,
+
+				pageDots: false,
+				autoPlay: 3000,
+				contain: true,
+			},
 		};
 	},
+	mounted() {},
 	methods: {
 		toggleMenuStatus() {
 			console.log("b", this.menuStatus);
 			this.menuStatus = !this.menuStatus;
+		},
+		next() {
+			this.$refs.flickity.next();
+		},
+
+		previous() {
+			this.$refs.flickity.previous();
 		},
 	},
 };
---
+++ src/pages/main/components/TheHero.vue
@@ -0,0 +1,102 @@
+<template>
+	<div class="hero">
+		<div class="slider">
+			<div>
+				<flickity
+					class="flickity slider"
+					ref="flickity"
+					:options="flickityOptions"
+				>
+					<div
+						v-for="(item, index) in slider"
+						:key="index"
+						class="carousel-cell slider__item"
+					>
+						<p class="slider__text">
+							Открытие 40-го Mobil 1 Center в г. Алматы
+						</p>
+						<img
+							:src="
+								require(`@/assets/img/pages/mainpage/${item}`)
+							"
+							alt=""
+							class="slider__img"
+						/>
+					</div>
+				</flickity>
+			</div>
+		</div>
+
+		<div class="podbor">
+			<div class="podbor__card">
+				<div>
+					<div class="podbor__title">Подбор по модели</div>
+					<div class="podbor__desc">
+						Подбор запчастей по модели автомобиля
+					</div>
+					<button class="podbor__btn">Попробовать</button>
+				</div>
+				<div>
+					<img
+						src="@/assets/img/pages/mainpage/blue-car-sellpage-audi@2x 1.svg"
+						alt=""
+					/>
+				</div>
+			</div>
+			<div class="podbor__card">
+				<div>
+					<div class="podbor__title">Подбор масла</div>
+					<div class="podbor__desc">
+						Подбор масел по VIN-коду и модели автомобиля
+					</div>
+					<button class="podbor__btn">Попробовать</button>
+				</div>
+				<div>
+					<img src="@/assets/img/pages/mainpage/maslo.svg" alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Flickity from "vue-flickity";
+
+export default {
+	name: "TheHero",
+	components: { Flickity },
+	data() {
+		return {
+			menuStatus: false,
+			slider: [
+				"Rectangle 28.png",
+				"Rectangle 28.png",
+				"Rectangle 28.png",
+			],
+			flickityOptions: {
+				prevNextButtons: true,
+				wrapAround: true,
+
+				pageDots: false,
+				autoPlay: 3000,
+			},
+		};
+	},
+	mounted() {},
+	methods: {
+		toggleMenuStatus() {
+			console.log("b", this.menuStatus);
+			this.menuStatus = !this.menuStatus;
+		},
+		next() {
+			this.$refs.flickity.next();
+		},
+
+		previous() {
+			this.$refs.flickity.previous();
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
