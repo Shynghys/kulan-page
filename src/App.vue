0593417e8f2edc4b77d8d95b0ada7384da050c07
Shynghys <shynghys@mail.ru>
2022-07-25 15:24:15 +0600

    fonts through google url

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,5 @@ export default {
 <style lang="scss">
 @import "./assets/scss/main";
 @import "./assets/fonts/stylesheet";
-@import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap");
 </style>
